2526c73d98c60752532954a42ec3f073a6b27c93
dbtjswhd <ysjong11@gmail.com>
2023-03-09 10:59:15 +0900

    Array.java 완성

---
+++ test_0308/src/Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] num = {94,85,95,88,90};
+		int max = 0;
+		int i=0;
+		for(i=0; i<num.length; i++) {
+			if(num[i]>max) {
+				max = num[i];
+			}
+		}
+		System.out.println(max);
+	}
+
+}
